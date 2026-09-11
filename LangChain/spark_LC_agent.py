2046df4e1ea97552f6427a569c9243972e12962d
lookup1 <1928996062@qq.com>
2026-09-11 16:26:31 +0800

    加了lanchainSession封装

--- LangChain/spark_LC_agent.py
+++ LangChain/spark_LC_agent.py
@@ -12,5 +12,22 @@ llm = ChatOpenAI(
     model = "spark-x2.5-4b",
 )
 
-response = llm.invoke("你是谁")
-print(response.content)
+# response = llm.invoke("你是谁")
+# print(response.content)
+
+#多轮对话Session封装
+from langchain_core.messages import (
+    AIMessage,  # 等价于OpenAI接口中的assistant role
+    HumanMessage, # 等价于OpenAI接口中的user role
+    SystemMessage, # 等价于OpenAI接口中的system role
+)
+
+messages = [
+    SystemMessage(content="你是AGIClass的课程助理"),
+    HumanMessage(content="我是学员，我叫李明"),
+    AIMessage(content="欢迎"),
+    HumanMessage(content="我是谁"),
+]
+
+ret = llm.invoke(messages)
+print(ret.content)
